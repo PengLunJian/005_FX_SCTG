7f574adac2e985cc29b79f6ec28c27725b5936eb
PengLunJian <875831103@qq.com>
2018-12-05 17:46:32 +0800

    市场推广登录提交

---
+++ src/mobile/js/login.js
@@ -0,0 +1,14 @@
+require(['jquery', 'common', 'template', 'fastclick', 'swiper'], function ($, common, template, fastclick, swiper) {
+
+    function LoginPage() {
+        var arguments = arguments.length ? arguments[0] : arguments;
+
+        this.constructor();
+    };
+
+    LoginPage.prototype.constructor = function () {
+    };
+
+    new LoginPage();
+
+});
