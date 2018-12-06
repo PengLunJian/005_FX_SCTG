3b0a1c98b3f164bc17951ed322db58110ff67ef4
PengLunJian <875831103@qq.com>
2018-12-06 17:43:52 +0800

    svg学习

--- src/mobile/js/login.js
+++ src/mobile/js/login.js
@@ -2,14 +2,31 @@ require(['jquery', 'common', 'template', 'fastclick', 'swiper'], function ($, co
 
     function LoginPage() {
         var arguments = arguments.length ? arguments[0] : arguments;
+        this.btnStart = arguments['btnStart'] ? arguments['btnStart'] : '.btn-start';
 
         this.constructor();
     };
 
     LoginPage.prototype.constructor = function () {
-        var oSvg = document.getElementById('circle');
-        console.log(oSvg.getTotalLength());
+        var oUpPath = document.querySelector('.icon-up');
+        var length = oUpPath.getTotalLength();
+        console.log(length);
     };
+
+    LoginPage.prototype.startTimer = function () {
+        var _this = this;
+        var initTime = 10;
+        $(document).on('click', this.btnStart, function () {
+            window.timer = setInterval(function () {
+                if (initTime === 0) {
+                    clearInterval(window.timer);
+                    return;
+                }
+                $(_this.btnStart).text('0' + (--initTime));
+            }, 1000);
+        });
+        return this;
+    }
 
     new LoginPage();
 
