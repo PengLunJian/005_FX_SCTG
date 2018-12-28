929ebfb1e5f20732092abcae6cbec993979e0d37
PengLunJian <875831103@qq.com>
2018-12-28 12:53:31 +0800

    1、人脸扫描页面制作 2、样式调整

---
+++ src/mobile/js/scanner.js
@@ -0,0 +1,15 @@
+require(['jquery', 'common', 'template'], function ($, common, template) {
+
+    function ScannerPage() {
+        var arguments = arguments.length ? arguments[0] : arguments;
+
+        this.constructor();
+    };
+
+    ScannerPage.prototype.constructor = function () {
+        return this;
+    };
+
+    new ScannerPage();
+
+});
